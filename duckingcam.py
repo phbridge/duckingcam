ef58ae3902f186c37b5ffac3789e6a884c4ec3b6
phbridge <bridges.p.j1@gmail.com>
2024-04-02 13:19:03 +0100

    no lock

--- duckingcam.py
+++ duckingcam.py
@@ -3,7 +3,8 @@ import cv2
 import threading
 
 app = Flask(__name__)
-lock = threading.Lock()
+lock0 = threading.Lock()
+lock2 = threading.Lock()
 
 
 @app.route('/stream0', methods=['GET'])
@@ -11,40 +12,24 @@ def stream0():
     return Response(generate(cam_index=0), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
-@app.route('/stream1', methods=['GET'])
-def stream1():
-    return Response(generate(cam_index=1), mimetype="multipart/x-mixed-replace; boundary=frame")
+# @app.route('/stream1', methods=['GET'])
+# def stream1():
+#     return Response(generate(cam_index=1), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 @app.route('/stream2', methods=['GET'])
 def stream2():
-    return Response(generate(cam_index=2), mimetype="multipart/x-mixed-replace; boundary=frame")
+    return Response(generate2(cam_index=2), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
-@app.route('/stream3', methods=['GET'])
-def stream3():
-    return Response(generate(cam_index=3), mimetype="multipart/x-mixed-replace; boundary=frame")
-
-
-@app.route('/')
-def index():
-    return """
-<body>
-<div class="container">
-    <div class="row">
-        <div class="col-lg-8  offset-lg-2">
-            <h3 class="mt-5">Live Streaming</h3>
-            <img src="/video_feed" width="100%">
-        </div>
-    </div>
-</div>
-</body>        
-    """
+# @app.route('/stream3', methods=['GET'])
+# def stream3():
+#     return Response(generate(cam_index=3), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 def generate(cam_index=0):
-    global lock
-    vc = cv2.VideoCapture(cam_index)
+    global lock0
+    vc = cv2.VideoCapture(0)
     # vc = cv2.VideoCapture(cam_index, cv2.CAP_DSHOW)
     # vc = cv2.VideoCapture(cam_index, cv2.CAP_V4L)
     # vc = cv2.VideoCapture(cam_index, cv2.CAP_V4L2)
@@ -63,6 +48,33 @@ def generate(cam_index=0):
             continue
         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
     vc.release()
+
+
+def generate2(cam_index=2):
+    global lock2
+    global encodedImage2
+    if not lock2.locked():
+        vc = cv2.VideoCapture(0)
+    # vc = cv2.VideoCapture(cam_index, cv2.CAP_DSHOW)
+    # vc = cv2.VideoCapture(cam_index, cv2.CAP_V4L)
+    # vc = cv2.VideoCapture(cam_index, cv2.CAP_V4L2)
+        if vc.isOpened():
+            rval, frame = vc.read()
+        else:
+            rval = False
+            print(" is false")
+        while rval:
+            # with lock:
+            rval, frame = vc.read()
+            if frame is None:
+                continue
+            (flag, encodedImage2) = cv2.imencode(".jpg", frame)
+            if not flag:
+                continue
+            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage2) + b'\r\n')
+        vc.release()
+    else:
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage2) + b'\r\n')
 
 
 if __name__ == '__main__':
