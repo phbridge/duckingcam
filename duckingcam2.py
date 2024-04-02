d74bf9b5f4f82b04518548b6a87849f4b01acf4e
phbridge <bridges.p.j1@gmail.com>
2024-04-02 17:41:07 +0100

    no lock

--- duckingcam2.py
+++ duckingcam2.py
@@ -94,7 +94,7 @@ debug = False
 options = None
 threaded = True
 processes = 4
-app.run(host, port, debug, processes=4)
+app.run(host, port, debug, threaded=False, processes=4)
 # http_server = wsgiserver.WSGIServer(host=host, port=port, wsgi_app=app)
 # http_server.start()
 
