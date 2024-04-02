893d81d54d0383814eec2ee120b802253328c8fd
phbridge <bridges.p.j1@gmail.com>
2024-04-02 13:57:52 +0100

    no lock

--- duckingcam2.py
+++ duckingcam2.py
@@ -1,49 +1,51 @@
 from flask import Flask, render_template, Response
 import cv2
+import threading
+
 
 app = Flask('hello')
-camera0 = cv2.VideoCapture(0)  # CAP_DSHOW because of https://answers.opencv.org/question/234933/opencv-440modulesvideoiosrccap_msmfcpp-682-cvcapture_msmfinitstream-failed-to-set-mediatype-stream-0-640x480-30-mfvideoformat_rgb24unsupported-media/
-camera2 = cv2.VideoCapture(2)
+
+
 image0 = bytes()
 image2 = bytes()
 
-
 def gen_frames0():
+    camera0 = cv2.VideoCapture(0)
     global image0
     while True:
         # print(time.time())
         success, _image0 = camera0.read()
         if not success:
             print(" not success")
-            yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image0 + b'\r\n')
         else:
             ret, buffer = cv2.imencode('.jpg', _image0)
             image0 = buffer.tobytes()
-            yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image0 + b'\r\n')
+
 
 
 def gen_frames2():
+    camera2 = cv2.VideoCapture(2)
     global image2
     while True:
         # print(time.time())
         success, _image2 = camera2.read()
         if not success:
             print(" not success")
-            yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image2 + b'\r\n')
         else:
             ret, buffer = cv2.imencode('.jpg', _image2)
             image2 = buffer.tobytes()
-            yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image2 + b'\r\n')
+
+            # yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image2 + b'\r\n')
 
 
 @app.route('/stream0')
 def stream0():
-    return Response(gen_frames0(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image0 + b'\r\n', mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/stream2')
 def stream2():
-    return Response(gen_frames2(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image2 + b'\r\n', mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 # @app.route('/')
@@ -61,6 +63,10 @@ def stream2():
 # </body>
 #     """
 
+cam0thread = threading.Thread(target=lambda: gen_frames0())
+cam0thread.start()
+cam2thread = threading.Thread(target=lambda: gen_frames2())
+cam2thread.start()
 
 
 host = "0.0.0.0"
