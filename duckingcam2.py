4d3bd5684f88c25473ec7288ee9da95689b011dd
phbridge <bridges.p.j1@gmail.com>
2024-04-02 15:16:26 +0100

    no lock

--- duckingcam2.py
+++ duckingcam2.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, Response
 import cv2
 import threading
 
-camera0 = cv2.VideoCapture(0, cv2.CAP_FFMPEG)
+camera0 = cv2.VideoCapture(0)
 camera2 = cv2.VideoCapture(2)
 print(camera0.get(cv2.CAP_PROP_FPS))
 print(camera2.get(cv2.CAP_PROP_FPS))
