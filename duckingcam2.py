02ecc2ff822ffec8b7802ebd3f34608a6513df10
phbridge <bridges.p.j1@gmail.com>
2024-04-02 16:35:19 +0100

    no lock

--- duckingcam2.py
+++ duckingcam2.py
@@ -1,91 +1,71 @@
 from flask import Flask, render_template, Response
 import cv2
-import threading
 
-cv2.CAP_GSTREAMER
 camera0 = cv2.VideoCapture(0)
+camera0.release()
 camera2 = cv2.VideoCapture(2)
 camera2.release()
-print(camera0.get(cv2.CAP_PROP_FPS))
-print(camera2.get(cv2.CAP_PROP_FPS))
-print(camera0.get(cv2.CAP_PROP_FRAME_WIDTH))
-print(camera0.get(cv2.CAP_PROP_FRAME_HEIGHT))
-print(camera2.get(cv2.CAP_PROP_FRAME_WIDTH))
-print(camera2.get(cv2.CAP_PROP_FRAME_HEIGHT))
-camera0.set(cv2.CAP_PROP_FPS, 30)
-camera2.set(cv2.CAP_PROP_FPS, 30)
-camera0.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-camera2.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-camera0.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-camera2.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-print(camera0.get(cv2.CAP_PROP_FPS))
-print(camera2.get(cv2.CAP_PROP_FPS))
-print(camera0.get(cv2.CAP_PROP_FRAME_WIDTH))
-print(camera0.get(cv2.CAP_PROP_FRAME_HEIGHT))
-print(camera2.get(cv2.CAP_PROP_FRAME_WIDTH))
-print(camera2.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 app = Flask('hello')
 
-image0 = bytes()
-image2 = bytes()
+
+# def gen_frames0():
+#     global image0
+#     while True:
+#         # print(time.time())
+#         success, _image0 = camera0.read()
+#         if not success:
+#             print("not success 0")
+#             yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image0 + b'\r\n')
+#         else:
+#             ret, buffer = cv2.imencode('.jpg', _image0)
+#             image0 = buffer.tobytes()
+#             yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image0 + b'\r\n')
 
 
-def gen_frames0():
-    global image0
+def read_frames(camera):
     while True:
-        # print(time.time())
-        success, _image0 = camera0.read()
-        if not success:
-            print("not success 0")
-            yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image0 + b'\r\n')
-        else:
-            ret, buffer = cv2.imencode('.jpg', _image0)
-            image0 = buffer.tobytes()
-            yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image0 + b'\r\n')
-
-
-def read_frames2():
-    while True:
-        # print(time.time())
-        success, _image2 = camera2.retrieve()
+        success, image = camera.retrieve()
         if not success:
             print("not success 2")
-            yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image2 + b'\r\n')
         else:
-            ret, buffer = cv2.imencode('.jpg', _image2)
-            _image2 = buffer.tobytes()
-            yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + _image2 + b'\r\n')
+            ret, buffer = cv2.imencode('.jpg', image)
+            image = buffer.tobytes()
+            yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
 
 
-def gen_frames2():
-    global image2
+def gen_frames(camera):
     while True:
-        # print(time.time())
-        success, _image2 = camera2.read()
+        success, image = camera.read()
         if not success:
             print("not success 2")
-            yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image2 + b'\r\n')
         else:
-            ret, buffer = cv2.imencode('.jpg', _image2)
-            image2 = buffer.tobytes()
-            yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image2 + b'\r\n')
+            ret, buffer = cv2.imencode('.jpg', image)
+            image = buffer.tobytes()
+            yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
 
 
 @app.route('/stream0')
 def stream0():
-    return Response(gen_frames0(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    print(camera0.isOpened())
+    if camera0.isOpened():
+        return Response(read_frames(camera=camera0), mimetype='multipart/x-mixed-replace; boundary=frame')
+    else:
+        camera0.open(0)
+        return Response(gen_frames(camera=camera0), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return "ERROR"
 
 
 @app.route('/stream2')
 def stream2():
+    global image2
     print(camera2.isOpened())
     if camera2.isOpened():
-        return Response(read_frames2(), mimetype='multipart/x-mixed-replace; boundary=frame')
+        return Response(read_frames(camera=camera2), mimetype='multipart/x-mixed-replace; boundary=frame')
     else:
         camera2.open(2)
-        return Response(gen_frames2(), mimetype='multipart/x-mixed-replace; boundary=frame')
-    return Response(gen_frames2(), mimetype='multipart/x-mixed-replace; boundary=frame')
+        return Response(gen_frames(camera=camera2), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return "ERROR"
 
 
 # @app.route('/')
