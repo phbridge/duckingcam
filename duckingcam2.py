2e696a4b39b2af3f572c20feed0832a6c4ba4733
phbridge <bridges.p.j1@gmail.com>
2024-04-03 11:44:51 +0100

    no lock

--- duckingcam2.py
+++ duckingcam2.py
@@ -1,31 +1,14 @@
-from flask import Flask, render_template, Response
+from flask import Flask, Response
 import cv2
 import wsgiserver
 import time
-import threading
 
 camera0 = cv2.VideoCapture(0, cv2.CAP_GSTREAMER)
 camera0.release()
 camera2 = cv2.VideoCapture(2, cv2.CAP_GSTREAMER)
 camera2.release()
 
-
 app = Flask('hello')
-
-# backup = [bytes(), bytes(), bytes()]
-
-# def gen_frames0():
-#     global image0
-#     while True:
-#         # print(time.time())
-#         success, _image0 = camera0.read()
-#         if not success:
-#             print("not success 0")
-#             yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image0 + b'\r\n')
-#         else:
-#             ret, buffer = cv2.imencode('.jpg', _image0)
-#             image0 = buffer.tobytes()
-#             yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image0 + b'\r\n')
 
 
 def read_frames(camera):
@@ -33,27 +16,12 @@ def read_frames(camera):
         time.sleep(0.1)
         success, image = camera.retrieve()
         if not success:
-            print("not success")
+            print("not success read")
         else:
             encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 50]
-            # ret, buffer = cv2.imencode('.jpg', image)
             ret, buffer = cv2.imencode('.jpg', image, encode_param)
             image = buffer.tobytes()
             yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
-
-        # ret, buffer = cv2.imencode('.jpg', backup[backup_int])
-        # image = buffer.tobytes()
-        # # yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
-        # yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + backup[backup_int] + b'\r\n')
-
-#
-# def gen_frames_only():
-#     while True:
-#         # success, image = camera0.grab()
-#         # success, image = camera2.grab()
-#         camera0.read()
-#         camera2.read()
-#         time.sleep(0.03)
 
 
 def gen_frames(camera):
@@ -61,14 +29,11 @@ def gen_frames(camera):
         time.sleep(0.1)
         success, image = camera.read()
         if not success:
-            print("not success")
+            print("not success gen")
         else:
             encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 95]
-            # ret, buffer = cv2.imencode('.jpg', image)
             ret, buffer = cv2.imencode('.jpg', image, encode_param)
-            # image = buffer.tobytes()
             image = buffer.tobytes()
-            # yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
             yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
 
 
@@ -76,11 +41,10 @@ def gen_frames(camera):
 def stream0():
     print(camera0.isOpened())
     if camera0.isOpened():
-        return Response(read_frames(camera=camera0), mimetype='multipart/x-mixed-replace; boundary=frame')
+        return Response(gen_frames(camera=camera0), mimetype='multipart/x-mixed-replace; boundary=frame')
     else:
         camera0.open(0)
         return Response(gen_frames(camera=camera0), mimetype='multipart/x-mixed-replace; boundary=frame')
-    # return Response(read_frames(camera=camera0), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/stream2')
@@ -91,21 +55,10 @@ def stream2():
     else:
         camera2.open(2)
         return Response(gen_frames(camera=camera2), mimetype='multipart/x-mixed-replace; boundary=frame')
-    return "ERROR"
-
-
-#
-# camgrab = threading.Thread(target=lambda: gen_frames_only())
-# camgrab.start()
 
 
 host = "0.0.0.0"
 port = 8000
-debug = False
-options = None
-threaded = True
-processes = 1
-# app.run(host=host, port=port, debug=debug)
 http_server = wsgiserver.WSGIServer(host=host, port=port, wsgi_app=app)
 http_server.start()
 
